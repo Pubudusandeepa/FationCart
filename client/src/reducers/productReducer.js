143cbad53572d29717e3632bb6e28b87b152e346
Pubudusandeepa <ppsandeepa12@gmail.com>
2020-10-22 00:18:26 +0530

    first commit

---
+++ client/src/reducers/productReducer.js
@@ -0,0 +1,22 @@
+import {FETCH_PRODUCTS,ORDER_PRODUCTS_BY_PRICE, FILTER_PRODUCTS_BY_SIZE} from '../types'
+
+export const productsReducer = (state = {}, action) => {
+    switch(action.type) {
+
+        case FILTER_PRODUCTS_BY_SIZE:
+            return {
+                ...state,
+                size: action.payload.size,
+                filteredItems: action.payload.items
+            }
+        case ORDER_PRODUCTS_BY_PRICE:
+            return {
+                ...state,
+                filteredItems: action.payload.items
+            }
+        case FETCH_PRODUCTS:
+            return {items: action.payload, filteredItems: action.payload}
+        default:
+            return state;    
+    }
+}
